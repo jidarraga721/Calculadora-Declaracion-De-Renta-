c8c347d544407767f85cc66bd057ccbbde7e0aa0
sparra07 <sammypm2017@gmail.com>
2026-02-18 07:30:20 -0500

    test

--- Pruebas unitarias/Logica_calculadora.py
+++ Pruebas unitarias/Logica_calculadora.py
@@ -1,4 +1,10 @@
 def cal_entradas(ingreso_bruto, Aportes_ley,Deducciones):
+    """
+    Calcula los impustos a pagar de una declaracion de renta
+    Ingreso_bruto: ingreso total que gan el usuario
+    Aportes_ley: Descuentos obligatorios exigidos por el estado
+    Deducciones: Gastos especificos que el usuario ya tiene o inversiones qeu hace para bajar legalmente la base de sus impuestos 
+    """
     Renta_liquida = ingreso_bruto - Aportes_ley
     Beneficio_real = Deducciones + ((Renta_liquida- Deducciones)*0.25)
     Limite_legal = Renta_liquida *0.40
