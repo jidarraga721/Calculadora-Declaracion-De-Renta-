57c902681515004a0c79176a7c2690fe8da2e7e9
sparra07 <sammypm2017@gmail.com>
2026-02-18 07:29:55 -0500

    Test
    Added extra test cases for the calculator logic to cover more scenarios.

--- Pruebas unitarias/test_calculadora.py
+++ Pruebas unitarias/test_calculadora.py
@@ -7,37 +7,68 @@ class Test_calculadora(unittest.TestCase):
         aportes_ley = 400000
         deducciones = 0 
 
-        Renta_liquida, Beneficio_real, Limite_legal, total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
 
         self.assertEqual(Renta_liquida, 4600000)
         self.assertEqual(Beneficio_real, 1150000)
         self.assertEqual(Limite_legal, 1840000)
-        self.assertEqual(total, 3450000)
+        self.assertEqual(Total, 3450000)
 
     def test_normal2(self): 
         ingreso_bruto = 8000000
         aportes_ley = 640000
         deducciones = 600000
 
-        Renta_liquida, Beneficio_real, Limite_legal, total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
 
         self.assertEqual(Renta_liquida, 7360000)
         self.assertEqual(Beneficio_real, 2290000)
         self.assertEqual(Limite_legal, 2944000)
-        self.assertEqual(total, 5070000)
+        self.assertEqual(Total, 5070000)
 
     def test_normal3(self): 
         ingreso_bruto = 12000000
         aportes_ley = 960000
         deducciones = 500000
 
-        Renta_liquida, Beneficio_real, Limite_legal, total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
-
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
         self.assertEqual(Renta_liquida, 11040000)
         self.assertEqual(Beneficio_real, 3135000)
         self.assertEqual(Limite_legal, 4416000)
-        self.assertEqual(total, 7905000)
+        self.assertEqual(Total, 7905000)
 
+    def test_extra1(self): 
+        ingreso_bruto = 1300000
+        aportes_ley = 104000
+        deducciones = 0
+
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+        self.assertEqual(Renta_liquida, 1196000)
+        self.assertEqual(Beneficio_real, 299000)
+        self.assertEqual(Limite_legal, 478400)
+        self.assertEqual(Total, 897000)
+
+    def test_extra2(self): 
+        ingreso_bruto = 20000000
+        aportes_ley = 1600000
+        deducciones = 12000000
+
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+        self.assertEqual(Renta_liquida, 18400000)
+        self.assertEqual(Beneficio_real, 13600000)
+        self.assertEqual(Limite_legal, 7360000)
+        self.assertEqual(Total, 11040000)
+    
+    def test_extra3(self): 
+        ingreso_bruto = 3500000
+        aportes_ley = 280000
+        deducciones = 0
+
+        Renta_liquida, Beneficio_real, Limite_legal, Total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+        self.assertEqual(Renta_liquida, 3220000)
+        self.assertEqual(Beneficio_real, 805000)
+        self.assertEqual(Limite_legal, 1288000)
+        self.assertEqual(Total, 2415000)
 
 
   
