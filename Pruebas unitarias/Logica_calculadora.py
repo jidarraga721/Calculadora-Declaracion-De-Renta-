69fc86294cb16ed91ef92c77e8619287cc9a8d69
sparra07 <sammypm2017@gmail.com>
2026-02-25 00:32:56 -0500

    pruebas de error
    Added validation checks for input parameters to ensure they are within acceptable ranges and raise exceptions for invalid values.

--- Pruebas unitarias/Logica_calculadora.py
+++ Pruebas unitarias/Logica_calculadora.py
@@ -5,6 +5,28 @@ def cal_entradas(ingreso_bruto, Aportes_ley,Deducciones):
     Aportes_ley: Descuentos obligatorios exigidos por el estado
     Deducciones: Gastos especificos que el usuario ya tiene o inversiones qeu hace para bajar legalmente la base de sus impuestos 
     """
+
+    if ingreso_bruto < 0:
+        raise Exception("El valor del ingreso Bruto debe ser mayor que cero")
+    
+    if ingreso_bruto == 0:
+        raise Exception("Debe tener ingresos")
+    
+    if Aportes_ley < 0:
+        raise Exception("El valor de los aportes debe ser mayor que cero")
+    
+    if Deducciones < 0:
+        raise Exception("El valor de las deducciones debe ser mayor que cero")
+    
+    if Deducciones > (ingreso_bruto-Aportes_ley):
+        raise Exception("Las deducciones estan fuera de rango")
+    
+    if Aportes_ley > ingreso_bruto:
+        raise Exception("Los aportes de ley no pueden ser mayores al ingreso bruto")
+    
+    if ingreso_bruto > 0 and Aportes_ley == 0:
+        raise Exception("Aportes de ley obligatorios faltantes")
+    
     Renta_liquida = ingreso_bruto - Aportes_ley
     Beneficio_real = Deducciones + ((Renta_liquida- Deducciones)*0.25)
     Limite_legal = Renta_liquida *0.40
