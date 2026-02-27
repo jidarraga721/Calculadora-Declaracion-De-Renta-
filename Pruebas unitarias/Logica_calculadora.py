52a4243c05b8dd07db031e1f2f636a264dc54e0c
sparra07 <sammypm2017@gmail.com>
2026-02-27 07:30:15 -0500

    Entrega 1

--- Pruebas unitarias/Logica_calculadora.py
+++ Pruebas unitarias/Logica_calculadora.py
@@ -1,3 +1,20 @@
+class ingreso_invalido( Exception):
+    pass
+
+class Aportes_fuera_rango( Exception):
+    pass
+
+class ingreso_0( Exception):
+    pass
+
+
+class Deduccion_fuera_rango( Exception):
+    pass
+
+
+class Aportes_obligatorios( Exception):
+    pass
+
 def cal_entradas(ingreso_bruto, Aportes_ley,Deducciones):
     """
     Calcula los impustos a pagar de una declaracion de renta
@@ -7,25 +24,19 @@ def cal_entradas(ingreso_bruto, Aportes_ley,Deducciones):
     """
 
     if ingreso_bruto < 0:
-        raise Exception("El valor del ingreso Bruto debe ser mayor que cero")
-    
-    if ingreso_bruto == 0:
-        raise Exception("Debe tener ingresos")
-    
-    if Aportes_ley < 0:
-        raise Exception("El valor de los aportes debe ser mayor que cero")
-    
-    if Deducciones < 0:
-        raise Exception("El valor de las deducciones debe ser mayor que cero")
-    
-    if Deducciones > (ingreso_bruto-Aportes_ley):
-        raise Exception("Las deducciones estan fuera de rango")
+        raise ingreso_invalido("El valor del ingreso Bruto debe ser mayor que cero")
     
     if Aportes_ley > ingreso_bruto:
-        raise Exception("Los aportes de ley no pueden ser mayores al ingreso bruto")
+        raise Aportes_fuera_rango("Los aportes de ley no pueden ser mayores al ingreso bruto")
+    
+    if ingreso_bruto == 0:
+        raise ingreso_0("El usuario debe de tener ingresos")
+    
+    if Deducciones > (ingreso_bruto-Aportes_ley):
+        raise Deduccion_fuera_rango("Las deducciones estan fuera de rango")
     
     if ingreso_bruto > 0 and Aportes_ley == 0:
-        raise Exception("Aportes de ley obligatorios faltantes")
+        raise Aportes_obligatorios("Aportes de ley obligatorios faltantes, todo asalariado debe aportar a salud y pension ")
     
     Renta_liquida = ingreso_bruto - Aportes_ley
     Beneficio_real = Deducciones + ((Renta_liquida- Deducciones)*0.25)
