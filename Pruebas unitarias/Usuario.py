77ea9bafdc64b29bfd1617c30b00c107e63fbdec
sparra07 <sammypm2017@gmail.com>
2026-03-11 07:02:24 -0500

    Fix print statements for clarity in Usuario.py

--- Pruebas unitarias/Usuario.py
+++ Pruebas unitarias/Usuario.py
@@ -12,9 +12,9 @@ try:
     Total=Logica_calculadora.cal_entradas(ingreso_bruto, Aportes_ley, Deducciones)
 
     print(f"la renta liquida tiene un valor de: {Renta_liquida[0]}")
-    print(f"El beneficio Real tiene unn valor de: {Beneficio_real[1]}")
-    print(f"El limite legalñ tiene un valor de: {Limite_legal[2]}")
-    print(f"El volor total de impuestos a pagar tiene un valor de:  {Total[3]}")         
+    print(f"El beneficio real tiene un valor de: {Beneficio_real[1]}")
+    print(f"El limite legal tiene un valor de: {Limite_legal[2]}")
+    print(f"El volor total de impuestos a pagar es:  {Total[3]}")         
 
 except Exception as err:
     print(f"Error: {err}")                                                                                                                 
