fb31c638f331e4f4f157d21b5d138277b6b69f8d
sparra07 <sammypm2017@gmail.com>
2026-03-19 00:35:20 -0500

    Consolo arreglo

--- src/view/Consola.py
+++ src/view/Consola.py
@@ -11,13 +11,13 @@ try:
     deducciones= int(input("Ingrese sus deducciones: "))
 
     # Llamado a la lógica de cálculo (Se obtiene la tupla completa en una sola ejecución)
-    renta_liquida, beneficio_real, limite_legal , total = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
+    resultado = Logica_calculadora.cal_entradas(ingreso_bruto, aportes_ley, deducciones)
     
     # Desglose de resultados para el usuario
-    print(f"la renta liquida tiene un valor de: {renta_liquida[0]}")
-    print(f"El beneficio real tiene un valor de: {beneficio_real[1]}")
-    print(f"El limite legal tiene un valor de: {limite_legal[2]}")
-    print(f"El volor total de impuestos a pagar es: {total[3]}")         
+    print(f"la renta liquida tiene un valor de: {resultado[0]}")
+    print(f"El beneficio real tiene un valor de: {resultado[1]}")
+    print(f"El limite legal tiene un valor de: {resultado[2]}")
+    print(f"El volor total de impuestos a pagar es: {resultado[3]}")         
 
 except Exception as err:
     """Captura y muestra cualquier error de validación o ejecución ocurrido durante el proceso."""
